3f8ebe73efbfd8841e72bb61575d9ec0ac4fc3d1
mikebasdeo <mikebasdeo@gmail.com>
2018-01-28 19:54:53 -0500

    Added lastLine and getLastLine for testing

--- src/point/of/sale/FakeDisplay.java
+++ src/point/of/sale/FakeDisplay.java
@@ -2,14 +2,19 @@ package point.of.sale;
 
 public class FakeDisplay implements Display {
 
+    String lastLine = "";
 
     public FakeDisplay() {
 
     }
 
+    @Override
     public void showLine(String line) {
-        System.out.println(line);
+        lastLine = line;
     }
 
+    public String getLastLine(){
+        return lastLine;
+    }
 
 }
