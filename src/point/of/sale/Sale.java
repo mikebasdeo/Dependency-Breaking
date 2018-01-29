fca60237178188c82802c0539b9c0f1dd6cdf704
mikebasdeo <mikebasdeo@gmail.com>
2018-01-28 19:19:02 -0500

    Added DB component

--- src/point/of/sale/Sale.java
+++ src/point/of/sale/Sale.java
@@ -2,15 +2,24 @@ package point.of.sale;
 
 public class Sale {
 
-    //Something we don't have access to
+    //Things we don't have access to
+    PostgresCalls postgres;
     ArtR56 artR56;
 
-    public Sale(ArtR56 artR56){
+
+
+    public Sale(ArtR56 artR56, PostgresCalls postgres){
         this.artR56 = artR56;
+        this.postgres = postgres;
     }
 
     public void scan(String barcode){
+
+        //display the barcode
         artR56.display(barcode);
+
+        //lookup barcode in postgres and get item
+        artR56.display(postgres.barcode(barcode));
     }
 
 
