6520655806da73d219ea560ee94b8a99d023de4f
mikebasdeo <mikebasdeo@gmail.com>
2018-01-28 20:59:16 -0500

    Testing display with hashStorage.

--- src/point/of/sale/TestSale.java
+++ src/point/of/sale/TestSale.java
@@ -17,8 +17,19 @@ public class TestSale {
     }
 
     @Test
-    public void testHashStorage()
-    {
+    public void testHashStorage() {
+        FakeDisplay fakeDisplay = new FakeDisplay();
+        HashStorage hashStorage = new HashStorage();
+
+        hashStorage.put("1", "Bread, 1.99");
+        hashStorage.put("2","Meat, 5.99");
+
+        Sale sale = new Sale(fakeDisplay, hashStorage);
+        sale.scan("1");
+        assertEquals("Bread, 1.99", fakeDisplay.getLastLine());
+
+        sale.scan("2");
+        assertEquals("Meat, 5.99", fakeDisplay.getLastLine());
 
     }
 
